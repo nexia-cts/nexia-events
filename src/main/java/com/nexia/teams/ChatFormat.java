13857dc6c31d1616b5a6716eed057fa92b0832a5
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-06-17 20:39:28 +0200

    yea

---
+++ src/main/java/com/nexia/teams/ChatFormat.java
@@ -0,0 +1,73 @@
+package com.nexia.teams;
+
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.chat.Component;
+
+/*
+ * Directly taken from Nexus and Nexia-Mod
+ * https://github.com/nexia-cts/nexus/blob/main/nexus-builder/src/main/java/com/nexia/nexus/builder/implementation/util/ObjectMappings.java#L3291
+ * https://github.com/nexia-cts/Nexia-Mod/blob/main/src/main/java/com/nexia/core/utilities/chat/ChatFormat.java
+ */
+
+public class ChatFormat {
+
+    public static RegistryAccess.Frozen provider;
+
+    public static TextColor brandColor1 = TextColor.fromHexString("#a400fc");
+
+    public static TextColor brandColor2 = TextColor.fromHexString("#e700f0");
+
+    public static TextColor arrowColor = TextColor.fromHexString("#4a4a4a");
+
+    public class Minecraft {
+        public static TextColor dark_red = TextColor.fromHexString("#b51413");
+
+        public static TextColor red = TextColor.fromHexString("#ff2b1c");
+
+        public static TextColor gold = TextColor.fromHexString("#f5bc42");
+
+        public static TextColor yellow = TextColor.fromHexString("#fff60c");
+
+        public static TextColor dark_green = TextColor.fromHexString("#1b9c19");
+
+        public static TextColor green = TextColor.fromHexString("#38e312");
+
+        public static TextColor aqua = TextColor.fromHexString("#28e2ff");
+
+        public static TextColor dark_aqua = TextColor.fromHexString("#399ca9");
+
+        public static TextColor dark_blue = TextColor.fromHexString("#263daa");
+
+        public static TextColor blue = TextColor.fromHexString("#2955ff");
+
+        public static TextColor light_purple = ChatFormat.brandColor2;
+
+        public static TextColor dark_purple = ChatFormat.brandColor1;
+
+        public static TextColor white = TextColor.fromHexString("#ffffff");
+
+        public static TextColor gray = TextColor.fromHexString("#c7c7c7");
+
+        public static TextColor dark_gray = TextColor.fromHexString("#767676");
+
+        public static TextColor black = TextColor.fromHexString("#252525");
+    }
+
+    public static net.kyori.adventure.text.Component nexiaMessage = MiniMessage.miniMessage().deserialize(String.format("<bold><gradient:%s:%s>Nexia</gradient></bold> <color:%s>»</color> ", ChatFormat.brandColor1, ChatFormat.brandColor2, ChatFormat.arrowColor));
+
+
+    public static Component convertComponent(net.kyori.adventure.text.Component component) {
+        if (component == null || provider == null) return null;
+        String compString = GsonComponentSerializer.gson().serialize(component);
+        return net.minecraft.network.chat.Component.Serializer.fromJson(compString, provider);
+    }
+
+    public static net.kyori.adventure.text.Component convertComponent(Component mcComponent) {
+        if (mcComponent == null || provider == null) return null;
+        String mcCompString = net.minecraft.network.chat.Component.Serializer.toJson(mcComponent, provider);
+        return GsonComponentSerializer.gson().deserialize(mcCompString);
+    }
+}
--- src/main/java/com/nexia/teams/NexiaTeams.java
+++ src/main/java/com/nexia/teams/NexiaTeams.java
@@ -1,10 +1,23 @@
 package com.nexia.teams;
 
+import com.google.common.base.Suppliers;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.kyori.adventure.text.Component;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.registries.BuiltInRegistries;
 
 public class NexiaTeams implements ModInitializer {
     @Override
     public void onInitialize() {
 
+        ServerLifecycleEvents.SERVER_STARTED.register((server -> {
+            ChatFormat.provider = Suppliers.ofInstance(RegistryAccess.fromRegistryOfRegistries(BuiltInRegistries.REGISTRY)).get();
+        }));
+
+        ServerPlayConnectionEvents.JOIN.register(((handler, sender, server) -> {
+            handler.getPlayer().sendSystemMessage(ChatFormat.convertComponent(ChatFormat.nexiaMessage.append(Component.text("hi"))));
+        }));
     }
 }
