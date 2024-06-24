2278805e4173abc37b376d9a8dc5eaec2b2dd4a6
surgeflame <flameninjasand@gmail.com>
2024-06-23 21:19:54 -0400

    bruh

--- src/main/java/com/nexia/teams/mixin/mace/MaceItemMixin.java
+++ src/main/java/com/nexia/teams/mixin/mace/MaceItemMixin.java
@@ -43,8 +43,8 @@ public class MaceItemMixin extends Item {
                 ServerLevel serverLevel = (ServerLevel)var10;
                 int x = EnchantmentHelper.getEnchantmentLevel(Enchantments.DENSITY, player);
                 float z = DensityEnchantment.calculateDamageAddition(x, player.fallDistance);
-                z = (float) (z / 4.2);
-                j = (float) (j / 2.1);
+                z = (float) (z / 2.8);
+                j = (float) (j / 1.4);
                 cir.setReturnValue(j + z);
             } else {
                 cir.setReturnValue(j);
