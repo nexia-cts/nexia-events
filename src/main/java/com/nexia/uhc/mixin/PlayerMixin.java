51573d49afbeaa40d7ed6b2cb5b8040973f115f0
Infinityy <120841985+Inf1nityy@users.noreply.github.com>
2024-08-14 04:12:03 +0200

    make shield disables play a sound

---
+++ src/main/java/com/nexia/uhc/mixin/PlayerMixin.java
@@ -0,0 +1,43 @@
+package com.nexia.uhc.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemCooldowns;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Player.class)
+public abstract class PlayerMixin extends LivingEntity {
+    protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Shadow public abstract ItemCooldowns getCooldowns();
+
+    /**
+     * @author NotCoded
+     * @reason Make shield break actually play the sound to other players.
+     */
+    @Overwrite
+    public boolean disableShield(float f) {
+        this.getCooldowns().addCooldown(Items.SHIELD, (int)(f * 20.0F));
+        this.stopUsingItem();
+        this.level.broadcastEntityEvent(this, (byte)30);
+        if (this.getLastDamageSource().getEntity() instanceof ServerPlayer attacker) {
+            SoundSource soundSource = null;
+            for (SoundSource source : SoundSource.values()) {
+                soundSource = source;
+            }
+            this.level.playSound(null, new BlockPos(attacker.position()), SoundEvents.SHIELD_BREAK, soundSource, 2f, 1f);
+        }
+        return true;
+    }
+}
